31775f34988c59805557e40adf77350de21dfbc2
Rebeca Bonilla <rebeca.bonilla.3011@gmail.com>
2026-04-14 13:28:47 -0500

    fix: vue config syntax and favicon path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/'
+  publicPath: '/',
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Debugger - Tu App'; // Aquí puedes cambiar el título
+      return args;
+    })
+  }
 })
