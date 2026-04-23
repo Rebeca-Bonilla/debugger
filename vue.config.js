8bcb84b627c3de2417c62b20a5dbaa5ecea5a011
OrbelinJimenezVazquez <200300671@ucaribe.edu.mx>
2026-04-23 17:01:40 -0500

    fix: adjust publicPath for multi-platform support (GitHub Pages vs Vercel)

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/debugger/'
+    ? (process.env.GITHUB_ACTIONS ? '/debugger/' : '/')
     : '/',
   chainWebpack: config => {
     config.plugin('html').tap(args => {
