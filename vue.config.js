6727c274e585b878f72a79b6083bf818faf23372
OrbelinJimenezVazquez <200300671@ucaribe.edu.mx>
2026-04-23 17:00:08 -0500

    ci: add github actions deployment workflow and fix publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/debugger/'
+    : '/',
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       args[0].title = 'Debugger';
